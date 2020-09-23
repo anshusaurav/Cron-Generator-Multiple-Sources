c618d471a9601b8a5f10a3647118831e6fa6a5a0
anshusaurav <anshu.saurav@gmail.com>
2020-09-23 13:57:23 +0530

    Detailed Reporting

--- server.js
+++ server.js
@@ -14,10 +14,12 @@ const timeStamp = () => {
 
 
 function remove(obj, key) {
+    console.log("Removing standup: " + key)
     obj[key].stop();
 }
 
 function removeAll(obj) {
+    console.log("Removing all standups: ")
     for (var key in obj) {
         if (obj.hasOwnProperty(key)) {
             obj[key].stop();
@@ -26,6 +28,7 @@ function removeAll(obj) {
 }
 
 function append(standups, standup, crons, taskStore) {
+    console.log("Adding standup: " + standup.id + " with cron:" + standup.cron)
     standups.push(standup);
     crons[standup.id] = new CronJob(standup.cron, () => {
         const uuid = uuidv4();
@@ -34,8 +37,13 @@ function append(standups, standup, crons, taskStore) {
         taskStore.push({ id: uuid, stamp, standupId: standup.id })
     }, null, true)
 }
-function printReport(taskArr) {
-    console.log("==============================Printing-Report=====================================\n\n\n\n")
+function printReport(taskArr, standups) {
+    console.log("\n\n==============================Printing-Reports=====================================\n\n");
+    standups.forEach(standup => {
+        console.log("For Standup:" + standup.id + " and cron:" + standup.cron);
+        taskArr.filter(task => task.standupId === standup.id).forEach(task => console.log(task.id + ' ' + task.stamp + ' ' + task.standupId));
+        console.log("\n")
+    })
     taskArr.forEach(task => console.log(task.id + ' ' + task.stamp + ' ' + task.standupId));
 }
 
@@ -59,7 +67,7 @@ standups.forEach((standup) => {
 setTimeout(remove, 1000 * 20, crons, 'Each-fifth-second');
 setTimeout(append, 1000 * 30, standups, { id: 'Each-two-second', cron: '0/2 * * * * *' }, crons, taskStore);
 setTimeout(removeAll, 1000 * 60, crons);
-setTimeout(printReport, 1000 * 65, taskStore);
+setTimeout(printReport, 1000 * 65, taskStore, standups);
 
 app.listen(4000, () => {
     console.log("Server started at: " + timeStamp())
