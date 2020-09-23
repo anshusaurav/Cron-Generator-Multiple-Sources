dea43c6691d00d71997ee948100fa9febb2068ca
anshusaurav <anshu.saurav@gmail.com>
2020-09-23 12:40:57 +0530

    Added log and more testing

--- server.js
+++ server.js
@@ -1,48 +1,66 @@
-var express = require("express")
-var CronJob = require('cron').CronJob;
-var app = express();
+const express = require("express")
+const CronJob = require('cron').CronJob;
+const app = express();
+const { v4: uuidv4 } = require('uuid');
+
+const timeStamp = () => {
+    var date = new Date();
+    var seconds = ("0" + date.getSeconds()).slice(-2);
+    var minutes = ("0" + date.getMinutes()).slice(-2);
+    var hour = ("0" + date.getHours()).slice(-2);
+    return `${hour}:${minutes}:${seconds}`
+}
 
 
-const standups = [
-    { id: 'Each fifth second', cron: '0/5 * * * * *' },
-    { id: 'Each eight second', cron: '0/8 * * * * *' },
-    { id: 'Each fourteen second', cron: '0/14 * * * * *' }
-];
+
+function remove(obj, key) {
+    obj[key].stop();
+}
+
+function removeAll(obj) {
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            obj[key].stop();
+        }
+    }
+}
+
+function append(standups, standup, crons, taskStore) {
+    standups.push(standup);
+    crons[standup.id] = new CronJob(standup.cron, () => {
+        const uuid = uuidv4();
+        const stamp = timeStamp();
+        console.log(stamp + ' ' + standup.id + ' ' + uuid);
+        taskStore.push({ id: uuid, stamp, standupId: standup.id })
+    }, null, true)
+}
+function printReport(taskArr) {
+    console.log("==============================Printing-Report=====================================\n\n\n\n")
+    taskArr.forEach(task => console.log(task.id + ' ' + task.stamp + ' ' + task.standupId));
+}
+
 const crons = {};
-
+const taskStore = [];
+const standups = [
+    { id: 'Each-fifth-second', cron: '0/5 * * * * *' },
+    { id: 'Each-eighth-second', cron: '0/8 * * * * *' },
+    { id: 'Each-fourteen-second', cron: '0/14 * * * * *' }
+];
 // Using cron
-standups.forEach((standup, index) => {
-    crons[index] = new CronJob(standup.cron, () => {
-
-        var date = new Date();
-        // date.setTime(result_from_Date_getTime);
-
-        var seconds = date.getSeconds();
-        var minutes = date.getMinutes();
-        var hour = date.getHours();
-        console.log(`${hour}:${minutes}:${seconds}` + standup.id)
-    }, () => { }, true)
+standups.forEach((standup) => {
+    crons[standup.id] = new CronJob(standup.cron, () => {
+        const uuid = uuidv4();
+        const stamp = timeStamp();
+        console.log(stamp + ' ' + standup.id + ' ' + uuid);
+        taskStore.push({ id: uuid, stamp, standupId: standup.id })
+    }, null, true)
 })
 
-setTimeout(remove, 1000 * 20, 0);
-setTimeout(append, 1000 * 15, standups, { id: 'Each two second', cron: '0/2 * * * * *' });
-function remove(index) {
-    crons[index + ''].stop();
-}
-
-function append(standups, standup) {
-    standups.push(standup);
-    crons[standups.length - 1] = new CronJob(standup.cron, () => {
-        var date = new Date();
-        // date.setTime(result_from_Date_getTime);
-
-        var seconds = date.getSeconds();
-        var minutes = date.getMinutes();
-        var hour = date.getHours();
-        console.log(`${hour}:${minutes}:${seconds}` + standup.id)
-    }, () => { }, true)
-}
+setTimeout(remove, 1000 * 20, crons, 'Each-fifth-second');
+setTimeout(append, 1000 * 30, standups, { id: 'Each-two-second', cron: '0/2 * * * * *' }, crons, taskStore);
+setTimeout(removeAll, 1000 * 60, crons);
+setTimeout(printReport, 1000 * 65, taskStore);
 
 app.listen(4000, () => {
-    console.log("Sever")
+    console.log("Server started at: " + timeStamp())
 });
